6dac34a6a948a33c4476143128d931784a04aabc
Alexey Gorchakov <amvrogor@gmail.com>
2023-02-27 22:12:41 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,22 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      const parsedContacts = JSON.parse(savedContacts);
+      this.setState({ contacts: parsedContacts });
+      return;
+    }
+    this.setState({ contacts: [] });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = newContact => {
     this.setState(prevState => {
       return prevState.contacts.find(
